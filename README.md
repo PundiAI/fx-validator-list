04f423ca021082fe6aed36378534347048ccb6c8
zakir <nash@wokoworks.com>
2022-01-10 16:00:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# fxCore Validator List
+
+* update fxcore validator.json
+
+```shell script
+# mainnet
+./run.sh mainnet
+# testnet
+./run.sh testnet
+```
---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [[ "$1" != "testnet" && "$1" != "mainnet" ]]; then
+  echo "Invalid networt, Please input ./run.sh testnet or ./run.sh mainnet" && exit 1
+fi
+
+node_url="https://fx-json.functionx.io:26657"
+if [ "$1" == "testnet" ]; then
+  node_url="https://testnet-fx-json.functionx.io:26657"
+fi
+
+echo "[]" >"./$1/validator.json"
+
+validators=$(fxcored query staking validators --node "$node_url" | jq '.validators')
+
+while read address moniker website contact; do
+  echo "$address $moniker"
+  mkdir -p "./$1/assets/$address" && touch "./$1/assets/$address/.gitkeep"
+  jq --argjson item "{
+    \"moniker\": \"$moniker\",
+    \"validatorAddress\": \"$address\",
+    \"website\": \"$website\",
+    \"contact\": \"$contact\"
+  }" '.+=[$item]' "./$1/validator.json" >"./$1/validator.tmp.json"
+  mv "./$1/validator.tmp.json" "./$1/validator.json"
+done < <(echo "$validators" | jq -r '.[]|"\(.operator_address) \(.description.moniker) \(.description.website) \(.description.security_contact)"')
