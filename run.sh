858e7dab1917a1f53eb4cf97df96e4c10bd3d7c8
zakir <nash@wokoworks.com>
2022-04-11 16:34:52 +0800

    feature: run.sh script add debug params

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ "$2" == "debug" ]; then
+  set -x
+fi
+
 if [[ "$1" != "testnet" && "$1" != "mainnet" ]]; then
   echo "Invalid networt, Please input ./run.sh testnet or ./run.sh mainnet" && exit 1
 fi
@@ -13,8 +17,9 @@ echo "[]" >"./$1/validator.json"
 
 validators=$(fxcored query staking validators --node "$node_url" | jq '.validators')
 
+index=1
 while read address moniker website contact; do
-  echo "$address $moniker"
+  echo $index "$address $moniker"
   mkdir -p "./$1/assets/$address" && touch "./$1/assets/$address/.gitkeep"
   jq --argjson item "{
     \"moniker\": \"$moniker\",
@@ -23,4 +28,5 @@ while read address moniker website contact; do
     \"contact\": \"$contact\"
   }" '.+=[$item]' "./$1/validator.json" >"./$1/validator.tmp.json"
   mv "./$1/validator.tmp.json" "./$1/validator.json"
+  index=$((index + 1))
 done < <(echo "$validators" | jq -r '.[]|"\(.operator_address) \(.description.moniker) \(.description.website) \(.description.security_contact)"')
